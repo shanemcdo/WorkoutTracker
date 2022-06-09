16288065590947221ab1f56b6f2de92b65103878
Shane McDonough <shanemcdman@gmail.com>
2022-06-08 21:25:19 -0700

    Make colors global variables

--- gui_workout_tracker.py
+++ gui_workout_tracker.py
@@ -6,13 +6,13 @@ from workout_tracker import WorkoutTracker
 from datetime import date
 import pygame, os
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 class GuiWorkoutTracker(WorkoutTracker):
     """Uses pygame to draw a calendar and display and record exersize data"""
-
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    GREEN = (0, 255, 0)
-    RED = (255, 0, 0)
 
     def __init__(self, filename: str = 'data.json', window_size: (int, int) = (700, 700), window_pos: (int, int) = (15, 30)):
         super().__init__(filename)
@@ -41,35 +41,35 @@ class GuiWorkoutTracker(WorkoutTracker):
 
     def draw_cross(self, pos: (int, int), size: (int, int), width: int = 1):
         """Draw a red cross at {pos} with a size of {size} and a width of {width}"""
-        pygame.draw.line(self.screen, self.RED, pos, (pos[0] + size[0], pos[1] + size[1]), width)
-        pygame.draw.line(self.screen, self.RED, (pos[0], pos[1] + size[1]), (pos[0] + size[0], pos[1]), width)
+        pygame.draw.line(self.screen, RED, pos, (pos[0] + size[0], pos[1] + size[1]), width)
+        pygame.draw.line(self.screen, RED, (pos[0], pos[1] + size[1]), (pos[0] + size[0], pos[1]), width)
 
     def draw_check(self, pos: (int, int), size: (int, int), width: int = 1):
         """Draw a green check at {pos} with a size of {size} and a width of {width}"""
         middle = (pos[0] + size[0] / 2, pos[1] + size[1] / 2)
-        pygame.draw.line(self.screen, self.GREEN, (pos[0], middle[1]), (middle[0], pos[1] + size[1]), width)
-        pygame.draw.line(self.screen, self.GREEN, (middle[0], pos[1] + size[1]), (pos[0] + size[0], pos[1]), width)
+        pygame.draw.line(self.screen, GREEN, (pos[0], middle[1]), (middle[0], pos[1] + size[1]), width)
+        pygame.draw.line(self.screen, GREEN, (middle[0], pos[1] + size[1]), (pos[0] + size[0], pos[1]), width)
 
     def draw_empty_calendar(self):
         """Draw an empty calendar"""
         top_line_height = self.window_size[1] / 10.5
         # draw horizontal lines
-        pygame.draw.line(self.screen, self.WHITE, (0, top_line_height), (self.window_size[0], top_line_height))
+        pygame.draw.line(self.screen, WHITE, (0, top_line_height), (self.window_size[0], top_line_height))
         for i in range(7):
             line_y = i * self.scale[1]
-            pygame.draw.line(self.screen, self.WHITE, (0, line_y), (self.window_size[0], line_y))
+            pygame.draw.line(self.screen, WHITE, (0, line_y), (self.window_size[0], line_y))
         # draw verticle lines
         for i in range(7):
             line_x = i * self.scale[0]
-            pygame.draw.line(self.screen, self.WHITE, (line_x, top_line_height), (line_x, self.window_size[1]))
+            pygame.draw.line(self.screen, WHITE, (line_x, top_line_height), (line_x, self.window_size[1]))
         # draw weekday names
         for i, weekday in enumerate(['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']):
-            text = self.day_font.render(weekday, True, self.WHITE)
+            text = self.day_font.render(weekday, True, WHITE)
             x_offset = self.scale[0] / 2 - text.get_size()[0] / 2
             self.screen.blit(text, (i * self.scale[0] + x_offset, top_line_height))
         # draw title
         title_string = self.selected_date.strftime('%B %Y')
-        text = self.title_font.render(title_string, True, self.WHITE)
+        text = self.title_font.render(title_string, True, WHITE)
         self.screen.blit(text, (self.window_size[0] / 2 - text.get_size()[0] / 2, 0))
 
     def keyboard_input(self, key: int):
@@ -128,13 +128,13 @@ class GuiWorkoutTracker(WorkoutTracker):
                 while d.isoweekday() % 7 != current_x:
                     current_x += 1
                 real_pos = self.calendar_to_window((current_x, current_y))
-                corner_number = self.corner_number_font.render(str(d.day), True, self.WHITE)
+                corner_number = self.corner_number_font.render(str(d.day), True, WHITE)
                 self.screen.blit(corner_number, (real_pos[0] + corner_offset[0], real_pos[1] + corner_offset[1]))
                 if d <= today:
                     self.days[current_y][current_x] = key
                     draw_func = self.draw_check if val else self.draw_cross
                     draw_func((real_pos[0] + pos_offset[0], real_pos[1] + pos_offset[1]), (self.scale[0] - size_offset[0], self.scale[1] - size_offset[1]), 10)
-                text = self.corner_number_font.render('Full Body' if d.isoweekday() in (1, 3, 5) else 'Cardio', True, self.WHITE)
+                text = self.corner_number_font.render('Full Body' if d.isoweekday() in (1, 3, 5) else 'Cardio', True, WHITE)
                 self.screen.blit(text, (real_pos[0] + self.scale[0] / 15, real_pos[1] + self.scale[1] * 0.75))
                 current_x += 1
                 if current_x > 6:
@@ -153,7 +153,7 @@ class GuiWorkoutTracker(WorkoutTracker):
                 streak += 1
             else:
                 streak = 0
-        self.screen.blit(self.day_font.render(f'Streak: {streak}', True, self.WHITE), (10, 10))
+        self.screen.blit(self.day_font.render(f'Streak: {streak}', True, WHITE), (10, 10))
 
     def run(self):
         """Run the main loop"""
@@ -166,7 +166,7 @@ class GuiWorkoutTracker(WorkoutTracker):
                     self.keyboard_input(event.key)
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     self.mouse_input()
-            self.screen.fill(self.BLACK)
+            self.screen.fill(BLACK)
             self.draw_empty_calendar()
             self.draw_days()
             self.draw_streak()
